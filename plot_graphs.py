f166ad7d02de25baa00a5e5d3802472fba61f3ef
marcus-elia <mselia@uvm.edu>
2021-03-06 10:42:31 -0500

    Moved the data generation into get_graph_data

--- plot_graphs.py
+++ plot_graphs.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from multiply import *
+#from multiply import *
+from get_graph_data import *
 import matplotlib.pyplot as plt
 import time
 import json
@@ -26,34 +27,6 @@ Arguments:
 -h        print usage string and exit
 """
 
-# -----------------------------
-# =============================
-#
-#      Helper Functions
-#
-# =============================
-# -----------------------------
-
-def format_time(x):
-    if x < 60:
-        return str(int(x)) + " seconds"
-    elif x < 3600:
-        return str(int(x//60)) + "." + str(int((x-60*(x//60))/60*10)) + " minutes"
-    else:
-        return str(int(x//3600)) + "." + str(int((x-3600*(x//3600))/3600*10)) + " hours"
-    
-def list_to_string(algorithm_list):
-    """ Converts a list of ints into a string with a dash between
-        each character"""
-    if not algorithm_list:
-        return "SB"
-    if len(algorithm_list) == 1:
-        return str(algorithm_list[0])
-    s = str(algorithm_list[0])
-    for n in algorithm_list[1:-1]:
-        s += '-'
-        s += str(n)
-    return s + '-' + str(algorithm_list[-1])
 
 # -----------------------------
 # =============================
@@ -62,54 +35,25 @@ def list_to_string(algorithm_list):
 #
 # =============================
 # -----------------------------
-def compare_decompositions(min_deg, max_deg, num_trials, 
+def plot_compare_decompositions(min_deg, max_deg, num_trials, 
                             algorithms,  interpolation,
                             filename=""):
-    algorithms = [tuple(algorithm_list) for algorithm_list in algorithms]
     
-    # the degrees (x-values)
-    degrees = range(min_deg, max_deg+1)
-    
-    # a dictionary mapping each algorithm list to a dictionary mapping each
-    # degree to the time sum for it
-    algorithm_to_time_sum = {algorithm_list: {degree:0 for degree in degrees} 
-                             for algorithm_list in algorithms}
-    
-    # this is the outer loop so all variables will be affected equally by
-    # slow outliers
-    progress_time = time.time() # for the progress bar
-    for _ in range(num_trials):
-        for degree in degrees:
-            f = [int(x) for x in np.random.randint(0, 2048, degree)]
-            g = [int(x) for x in np.random.randint(0, 2048, degree)]
-            for algorithm_list in algorithms:
-                start_time = time.process_time()
-                multiply(f, g, algorithm_list, interpolation)
-                total_time = time.process_time() - start_time
-                algorithm_to_time_sum[algorithm_list][degree] += total_time
-                
-        # progress bar
-        if _ % (num_trials//10) == 0:
-            x = _ // (num_trials//10)
-            this_time = format_time(time.time()-progress_time)
-            print("[" + "-"*x + " "*(10-x) + "]  " + str(this_time))
-    print("[----------]  " + str(format_time(time.time()-progress_time)))
-                
-    # take the average
-    algorithm_list_to_y_values = {algorithm_list :
-        [algorithm_to_time_sum[algorithm_list][degree]/num_trials for 
-         degree in degrees] for algorithm_list in algorithms}
+    # Run the trials to get data
+    algorithm_list_to_y_values = compare_decompositions(min_deg, max_deg, num_trials, 
+                            algorithms,  interpolation)
     
     # now plot
+    degrees = range(min_deg, max_deg + 1)
     plt.figure(figsize=(16,8))
-    for algorithm_list in algorithm_to_time_sum:
+    for algorithm_list in algorithm_list_to_y_values:
         plt.plot(degrees, algorithm_list_to_y_values[algorithm_list], 
                  lw=4, label=list_to_string(algorithm_list))
 
     plt.xlabel("Degree", size=19)
     plt.ylabel("Average CPU Time (seconds)", size=19)
     plt.legend(fontsize=18)
-    plt.title("Running Time of Toom-Cook Decompositions", size=20)
+    plt.title("Comparison of Toom-Cook Decompositions", size=20)
     if filename != "":
         plt.savefig(filename, bbox_inches='tight')
     else:
@@ -122,48 +66,18 @@ def compare_decompositions(min_deg, max_deg, num_trials,
     
     return
 
-def compare_interpolations(min_deg, max_deg, num_trials, 
+def plot_compare_interpolations(min_deg, max_deg, num_trials, 
                             algorithm_list,
                             filename=""):
-    #algorithms = [tuple(algorithm_list) for algorithm_list in algorithms]
-    interpolations = ("Natural", "Efficient", "Matrix")
     
-    # the degrees (x-values)
-    degrees = range(min_deg, max_deg+1)
-    
-    # a dictionary mapping each algorithm list to a dictionary mapping each
-    # degree to the time sum for it
-    interpolation_to_time_sum = {interpolation: {degree:0 for degree in degrees} 
-                             for interpolation in interpolations}
-    
-    # this is the outer loop so all variables will be affected equally by
-    # slow outliers
-    progress_time = time.time() # for the progress bar
-    for _ in range(num_trials):
-        for degree in degrees:
-            f = [int(x) for x in np.random.randint(0, 2048, degree)]
-            g = [int(x) for x in np.random.randint(0, 2048, degree)]
-            for interpolation in interpolations:
-                start_time = time.process_time()
-                multiply(f, g, algorithm_list, interpolation)
-                total_time = time.process_time() - start_time
-                interpolation_to_time_sum[interpolation][degree] += total_time
-                
-        # progress bar
-        if _ % (num_trials//10) == 0:
-            x = _ // (num_trials//10)
-            this_time = format_time(time.time()-progress_time)
-            print("[" + "-"*x + " "*(10-x) + "]  " + str(this_time))
-    print("[----------]  " + str(format_time(time.time()-progress_time)))
-                
-    # take the average
-    interpolation_to_y_values = {interpolation :
-        [interpolation_to_time_sum[interpolation][degree]/num_trials for 
-         degree in degrees] for interpolation in interpolations}
+    # Run the trials to get data
+    interpolation_to_y_values = compare_interpolations(min_deg, max_deg, num_trials, 
+                            algorithm_list)
     
     # now plot
+    degrees = range(min_deg, max_deg + 1)
     plt.figure(figsize=(16,8))
-    for interpolation in interpolation_to_time_sum:
+    for interpolation in interpolation_to_y_values:
         plt.plot(degrees, interpolation_to_y_values[interpolation], 
                  lw=4, label=interpolation)
 
@@ -240,11 +154,11 @@ def main(argv):
         sys.exit(1)
     
     if len(algorithm_list) >= 1 and interpolation_method != "":
-        compare_decompositions(min_degree, max_degree, num_trials, 
+        plot_compare_decompositions(min_degree, max_degree, num_trials, 
                             algorithm_list, interpolation_method,
                             filename=input_filename)
     elif len(algorithm_list) == 1 and interpolation_method == "":
-        compare_interpolations(min_degree, max_degree, num_trials, 
+        plot_compare_interpolations(min_degree, max_degree, num_trials, 
                                algorithm_list[0],
                                filename=input_filename)
     else:
